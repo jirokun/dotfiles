26148587b189c647a38e63e3bdac41c08c967a43
Jiro Iwamoto <jirokun@gmail.com>
2015-12-29 18:31:23 +0900

    add gitc command and etc...

---
+++ .shell/myzshrc.sh
@@ -0,0 +1,22 @@
+function repos() {
+  cd $REPOS/$(ls $REPOS | fzf)
+}
+function fzf_history() {
+  CMD=`history -E 1 | fzf | cut -d " " -f 2`
+  echo
+  print -z $CMD
+}
+function fzf-history-selection() {
+  BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | fzf`
+  CURSOR=$#BUFFER
+  zle reset-prompt
+}
+
+
+alias ssh="~/.ghq/github.com/jirokun/dotfiles/.shell/ssh-host-color-for-iterm2.sh"
+alias ll="ls -lF"
+
+zle -N fzf-history-selection
+bindkey '^r' fzf-history-selection
+
+PATH=$PATH:~/.ghq/github.com/jirokun/dotfiles/.shell
